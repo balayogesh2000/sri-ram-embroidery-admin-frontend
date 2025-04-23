6edf3177a703b572f6461f8f75a5180a46dc448a
Bala Yogesh <balayogesh2000@gmail.com>
2025-04-23 22:19:25 +0530

    axios loader timer removed

--- src/utils/addLoaderToAxios.js
+++ src/utils/addLoaderToAxios.js
@@ -1,55 +1,41 @@
-import { toast } from "react-toastify";
-
 import { showLoader, hideLoader } from "@/components/Loader";
 
 const addLoaderToAxios = (axios) => {
-  let timer;
   let numOfCalls = 0;
 
   axios.interceptors.request.use(
     function (config) {
-      numOfCalls++;
-
       if (!config.skipLoader) {
-        if (timer) {
-          clearTimeout(timer);
+        if (numOfCalls === 0) {
+          showLoader();
         }
-        showLoader();
-        timer = setTimeout(() => {
-          hideLoader();
-          toast.warn("Server is taking longer time than expected");
-        }, 10000);
+        numOfCalls++;
       }
-
       return config;
     },
     function (error) {
-      numOfCalls--;
-      if (timer) {
-        clearTimeout(timer);
-      }
-      hideLoader();
       return Promise.reject(error);
     }
   );
 
   axios.interceptors.response.use(
     function (response) {
-      numOfCalls--;
-      if (timer) {
-        clearTimeout(timer);
-      }
-      if (numOfCalls === 0 && !response.config.skipLoader) {
-        hideLoader();
+      if (!response.config.skipLoader) {
+        numOfCalls--;
+        if (numOfCalls === 0) {
+          hideLoader();
+        }
       }
       return response;
     },
     function (error) {
-      numOfCalls--;
-      if (timer) {
-        clearTimeout(timer);
+      const config = error.config || {};
+      if (!config.skipLoader) {
+        numOfCalls--;
+        if (numOfCalls === 0) {
+          hideLoader();
+        }
       }
-      hideLoader();
       return Promise.reject(error);
     }
   );
